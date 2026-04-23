7a3c6f2e667f0d7d2ceb1df26443f4d4a9974207
Ryoma Hara <toooooom720@gmail.com>
2026-04-23 12:34:30 +0900

    feat: add search form

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,40 @@
-import { getPokemon } from './api.ts'
+import { getPokemon } from './api.ts';
 
-const pokemon = await getPokemon(720);
+const form = document.getElementById('poke-form');
 
-// 表示領域の取得
-const container = document.getElementById('poke-container');
+form?.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const input = document.getElementById('poke-id');
 
-// 表示要素生成
-const nameElem = document.createElement('p');
-nameElem.textContent = `Name : ${pokemon.name}`;
+  if (!(input instanceof HTMLInputElement)) {
+    return;
+  }
+  const id = input.value;
 
-const image = document.createElement('img');
-image.src = pokemon.sprites.front_default;
-image.alt = pokemon.name;
+  const nameElem = document.getElementById('poke-name');
+  const imageElem = document.getElementById('poke-image') as HTMLImageElement;
+  const errorElem = document.getElementById('error-msg');
+  if (nameElem) nameElem.textContent = '';
+  if (imageElem) {
+    imageElem.src = '';
+    imageElem.alt = '';
+  }
+  if (errorElem) errorElem.textContent = '';
 
-// 要素をDOMへ追加
-container?.appendChild(nameElem);
-container?.appendChild(image);
+  try {
+    const pokemon = await getPokemon(Number(id));
 
+    if (nameElem) {
+      nameElem.textContent = `Name : ${pokemon.name}`;
+    }
+    if (imageElem) {
+     imageElem.src = pokemon.sprites.front_default;
+     imageElem.alt = pokemon.name;
+    }
+
+  } catch (error) {
+    if (errorElem) {
+      errorElem.textContent = 'Not found';
+    }
+  }
+});
