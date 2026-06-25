3f2ae24554075bb22e16ed4561acf0098de1ad79
Ryoma Hara <toooooom720@gmail.com>
2026-06-25 21:40:48 +0900

    feat: 種族値の各パラメーターを画面へ表示

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,15 @@ form?.addEventListener('submit', async (event) => {
   const nameElem = document.getElementById('poke-name');
   const imageElem = document.getElementById('poke-image') as HTMLImageElement;
   const errorElem = document.getElementById('error-msg');
+
+  // stats element
+  const hStatsElem = document.getElementById('stats-h');
+  const aStatsElem = document.getElementById('stats-a');
+  const bStatsElem = document.getElementById('stats-b');
+  const cStatsElem = document.getElementById('stats-c');
+  const dStatsElem = document.getElementById('stats-d');
+  const sStatsElem = document.getElementById('stats-s');
+
   if (nameElem) nameElem.textContent = '';
   if (imageElem) {
     imageElem.src = '';
@@ -27,9 +36,32 @@ form?.addEventListener('submit', async (event) => {
     if (nameElem) {
       nameElem.textContent = `Name : ${pokemon.name}`;
     }
+
     if (imageElem) {
      imageElem.src = pokemon.sprites.front_default;
      imageElem.alt = pokemon.name;
+    }
+
+    const statsAry = pokemon.stats;
+    if (5 < statsAry.length) {
+      if (hStatsElem) {
+        hStatsElem.textContent = `HP : ${String(pokemon.stats[0]['base_stat'])}`;
+      }
+      if (aStatsElem) {
+        aStatsElem.textContent = `Attack : ${String(pokemon.stats[1]['base_stat'])}`;
+      }
+      if (bStatsElem) {
+        bStatsElem.textContent = `Defence : ${String(pokemon.stats[2]['base_stat'])}`;
+      }
+      if (cStatsElem) {
+        cStatsElem.textContent = `S-Attack : ${String(pokemon.stats[3]['base_stat'])}`;
+      }
+      if (dStatsElem) {
+        dStatsElem.textContent = `S-Defence : ${String(pokemon.stats[4]['base_stat'])}`;
+      }
+      if (sStatsElem) {
+        sStatsElem.textContent = `Speed : ${String(pokemon.stats[5]['base_stat'])}`;
+      }
     }
 
   } catch (error) {
